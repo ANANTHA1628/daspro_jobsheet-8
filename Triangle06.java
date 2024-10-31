426de0181005343c7b61b25e3e6e3b395d8176d1
ANANTHA1628 <aryaputraferdiananta@gmail.com>
2024-10-31 19:09:10 +0700

    Triangle06

---
+++ Triangle06.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Triangle06 {
+
+    public static void main(String[] args) {
+        Scanner input06 =new Scanner(System.in);
+        System.out.print("masukkan nilai N = ");
+        int N = input06.nextInt();
+        int i = 0;
+        while (i <= N){
+            int j = 0;
+            while (j < i ) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+        }
+            
+        
+    }
+}
